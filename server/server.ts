a3da52acd35736a56a7194505ac55e4ba57bc55c
Cundus <sundusardian@gmail.com>
2023-11-05 01:41:16 +0700

    add express json

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
-
+app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
    res.send("Hello, Express with TypeScript!");
 });
